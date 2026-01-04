a40698a19b9ed1cf9a95a4a951c311733f686edc
Attila Domokos <adomokos@gmail.com>
2026-01-04 14:52:39 -0600

    Remove rake completion

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -6,4 +6,3 @@ zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-
 zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
 zstyle ':completion:*:*:(ssh|scp):*:*' hosts `sed 's/^\([^ ,]*\).*$/\1/' ~/.ssh/known_hosts`
 
-source ~/.zsh/rake_completions.zsh
